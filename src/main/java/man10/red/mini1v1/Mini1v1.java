5f4aec4d9c94b910763c7da4a66d0ea993f9a988
BuildTools <unconfigured@null.spigotmc.org>
2017-04-05 16:40:24 +0900

    Initial commit

---
+++ src/main/java/man10/red/mini1v1/Mini1v1.java
@@ -0,0 +1,17 @@
+package man10.red.mini1v1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mini1v1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
